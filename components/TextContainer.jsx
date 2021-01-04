05f96f9adbf353dff0dab6600b8f850d6fe961d6
zzacong <ongzz100@gmail.com>
2021-01-04 18:37:16 +1100

    chat function

---
+++ components/TextContainer.jsx
@@ -0,0 +1,49 @@
+import Image from 'next/image'
+import styles from '../styles/TextContainer.module.css'
+
+export default function TextContainer({ users }) {
+  return (
+    <div className={styles.textContainer}>
+      <div>
+        <h1>
+          Realtime Chat Application{' '}
+          <span role="img" aria-label="emoji">
+            💬
+          </span>
+        </h1>
+        <h3>
+          Created with React, Express, Node and Socket.IO{' '}
+          <span role="img" aria-label="emoji">
+            ❤️
+          </span>
+        </h3>
+        <h3>
+          Try it out right now!{' '}
+          <span role="img" aria-label="emoji">
+            ⬅️
+          </span>
+        </h3>
+      </div>
+      {users && (
+        <div>
+          <h2>People currently chatting:</h2>
+          <div className={styles.activeContainer}>
+            <ul className={styles.list}>
+              {users.map(({ name }) => (
+                <li key={name} className={styles.activeItem}>
+                  <h3>{name}</h3>
+                  <Image
+                    src="/onlineIcon.png"
+                    alt="Online Icon"
+                    width={8}
+                    height={8}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
--- pages/chat.jsx
+++ pages/chat.jsx
@@ -1,32 +1,75 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
-import io from 'socket.io-client'
+import { io } from 'socket.io-client'
+import styles from '../styles/Chat.module.css'
+import InfoBar from '../components/InfoBar'
+import Input from '../components/Input'
+import Messages from '../components/Messages'
+import TextContainer from '../components/TextContainer'
 
 let socket
 
 export default function Chat() {
   const [name, setName] = useState('')
   const [room, setRoom] = useState('')
-  const { query } = useRouter()
-  const ENDPOINT = 'http://localhost:5000'
+  const [users, setUsers] = useState('')
+  const [message, setMessage] = useState('')
+  const [messages, setMessages] = useState([])
+  const router = useRouter()
 
   useEffect(() => {
-    const { name, room } = query
+    socket = io('http://localhost:5000')
 
-    socket = io(ENDPOINT)
-
+    const { name, room } = router.query
     setName(name)
     setRoom(room)
 
-    socket.emit('join', { name, room }, ({ error }) => {
-      if (error) alert(error)
+    socket.emit('join', { name, room }, error => {
+      if (error) {
+        alert(error)
+        router.push('/')
+      }
+    })
+
+    socket.on('connect', () => {
+      console.log(`Connected on ${socket.id}`)
+    })
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected')
+    })
+
+    socket.on('message', message => {
+      console.log(message)
+      setMessages(prev => [...prev, message])
+    })
+
+    socket.on('roomData', ({ users }) => {
+      setUsers(users)
     })
 
     return () => {
       socket.disconnect()
-      socket.off()
     }
-  }, [ENDPOINT, query])
+  }, [router.query])
 
-  return <div>Chat</div>
+  const sendMessage = e => {
+    e.preventDefault()
+    if (message) socket.emit('sendMessage', message, () => setMessage(''))
+  }
+
+  return (
+    <div className={styles.outerContainer}>
+      <div className={styles.container}>
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  )
 }
