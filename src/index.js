53cb669dda20d21b83c49e86cc07a9011eeb30d7
zzacong <ongzz100@gmail.com>
2021-01-05 00:44:26 +1100

    allow vercel cors

--- src/index.js
+++ src/index.js
@@ -12,7 +12,10 @@ app.use(router)
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000',
+    origin: [
+      'http://localhost:3000',
+      'https://realtime-chat-socketio-client.vercel.app',
+    ],
     methods: ['GET', 'POST'],
     credentials: true,
   },
